208801f958b9b9168e5519d53c71ac3aa30c73e7
Ainaa Azman <ainaasakinah@gmail.com>
2021-08-02 17:42:04 +0900

    refactor: change from using sessions to using a database

--- todo.rb
+++ todo.rb
@@ -1,12 +1,18 @@
 require "sinatra"
-require "sinatra/reloader"
 require "sinatra/content_for"
 require "tilt/erubis"
+
+require_relative "database_persistance"
 
 configure do
   enable :sessions
   set :session_secret, 'secret'
   set :erb, :escape_html => true
+end
+
+configure(:development) do
+  require "sinatra/reloader"
+  also_reload "database_persistance.rb"
 end
 
 helpers do
@@ -41,75 +47,14 @@ helpers do
   end
 end
 
-class SessionPersistance
-  def initialize(session)
-    @session = session
-    @session[:lists] ||= []
-  end
+def load_list(id)
+  list = @storage.find_list(id)
+  return list if list
 
-  def find_list(id)
-    @session[:lists].find{ |list| list[:id] == id }
-  end
-
-  def all_lists
-    @session[:lists]
-  end
-
-  def create_new_list(list_name)
-    id = next_element_id(@session[:lists])
-    @session[:lists] << { id: id, name: list_name, todos: [] }
-  end
-
-  def delete_list(id)
-    @session[:lists].reject! { |list| list[:id] == id }
-  end
-
-  def update_list_name(id, new_name)
-    list = find_list(id)
-    list[:name] = new_name
-  end
-
-  def create_new_todo(list_id, todo_name)
-    list = find_list(list_id)
-    id = next_element_id(list[:todos])
-    list[:todos] << { id: id, name: todo_name, completed: false }
-  end
-
-  def delete_todo_from_list(list_id, todo_id)
-    list = find_list(list_id)
-    list[:todos].reject! { |todo| todo[:id] == todo_id }
-  end
-
-  def update_todo_status(list_id, todo_id, new_status)
-    list = find_list(list_id)
-    todo = list[:todos].find { |t| t[:id] == todo_id }
-    todo[:completed] = new_status
-  end
-
-  def mark_all_todos_as_completed(list_id)
-    list = find_list(list_id)
-
-    list[:todos].each do |todo|
-      todo[:completed] = true
-    end
-  end
-
-  private
-
-  def next_element_id(elements)
-    max = elements.map { |todo| todo[:id] }.max || 0
-    max + 1
-  end
+  session[:error] = "The specified list was not found."
+  redirect "/lists"
+  halt
 end
-
-  def load_list(id)
-    list = @storage.find_list(id)
-    return list if list
-
-    session[:error] = "The specified list was not found."
-    redirect "/lists"
-    halt
-  end
 
 # Return an error message if the name is invalid. Return nil if name is valid.
 def error_for_list_name(name)
@@ -128,7 +73,7 @@ def error_for_todo(name)
 end
 
 before do
-  @storage = SessionPersistance.new(session)
+  @storage = DatabasePersistance.new(logger)
 end
 
 get "/" do
